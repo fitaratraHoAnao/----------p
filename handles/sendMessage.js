8d9f4b27a253d864c4f4e5a9c630e68d8bb82737
Brunorakotomalala-espa <brunorakotomalala113@gmail.com>
2024-09-18 23:49:26 +0300

    Update sendMessage.js

--- handles/sendMessage.js
+++ handles/sendMessage.js
@@ -1,43 +1,39 @@
 const request = require('request');
 
 function sendMessage(senderId, message, pageAccessToken) {
-  if (!message || (!message.text && !message.attachment && !message.quick_replies)) {
-    console.error('Error: Message must provide valid text, attachment, or quick replies.');
-    return;
-  }
+  if (!message || (!message.text && !message.attachment)) {
+    console.error('Error: Message must provide valid text or attachment.');
+    return;
+  }
 
-  const payload = {
-    recipient: { id: senderId },
-    message: {}
-  };
+  const payload = {
+    recipient: { id: senderId },
+    message: {}
+  };
 
-  if (message.text) {
-    payload.message.text = message.text;
-  }
+  if (message.text) {
+    payload.message.text = message.text;
+  }
 
-  if (message.attachment) {
-    payload.message.attachment = message.attachment;
-  }
+  if (message.attachment) {
+    payload.message.attachment = message.attachment;
+  }
 
-  // Si les quick replies sont présents, les ajouter au payload
-  if (message.quick_replies) {
-    payload.message.quick_replies = message.quick_replies;
-  }
-
-  request({
-    url: 'https://graph.facebook.com/v13.0/me/messages',
-    qs: { access_token: pageAccessToken },
-    method: 'POST',
-    json: payload,
-  }, (error, response, body) => {
-    if (error) {
-      console.error('Error sending message:', error);
-    } else if (response.body.error) {
-      console.error('Error response:', response.body.error);
-    } else {
-      console.log('Message sent successfully:', body);
-    }
-  });
+  request({
+    url: 'https://graph.facebook.com/v13.0/me/messages',
+    qs: { access_token: pageAccessToken },
+    method: 'POST',
+    json: payload,
+  }, (error, response, body) => {
+    if (error) {
+      console.error('Error sending message:', error);
+    } else if (response.body.error) {
+      console.error('Error response:', response.body.error);
+    } else {
+      console.log('Message sent successfully:', body);
+    }
+  });
 }
 
 module.exports = { sendMessage };
+
