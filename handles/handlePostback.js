ce8f39bd4beb20c583b66ce70512b3ea6f2b56c0
Brunorakotomalala-espa <brunorakotomalala113@gmail.com>
2024-09-18 22:20:18 +0300

    Update handlePostback.js

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -4,8 +4,20 @@ function handlePostback(event, pageAccessToken) {
   const senderId = event.sender.id;
   const payload = event.postback.payload;
 
-  // Send a message back to the sender
-  sendMessage(senderId, { text: `You sent a postback with payload: ${payload}` }, pageAccessToken);
+  // Vérifier et exécuter des actions en fonction du payload
+  switch (payload) {
+    case 'traduction fr':
+      sendMessage(senderId, { text: 'Entrez le texte à traduire en Français.' }, pageAccessToken);
+      break;
+    case 'traduction en':
+      sendMessage(senderId, { text: 'Enter the text to translate to English.' }, pageAccessToken);
+      break;
+    case 'traduction es':
+      sendMessage(senderId, { text: 'Ingrese el texto para traducir al Español.' }, pageAccessToken);
+      break;
+    default:
+      sendMessage(senderId, { text: `Payload reçu : ${payload}` }, pageAccessToken);
+  }
 }
 
 module.exports = { handlePostback };
