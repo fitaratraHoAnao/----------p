f7cb5af313357a9087293e386fe77d9df96889e8
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2024-10-29 10:24:51 +0300

    Update uid.js

--- commands/uid.js
+++ commands/uid.js
@@ -6,9 +6,16 @@ module.exports = async (senderId, prompt) => {
         // Envoyer un message de confirmation que le message a été reçu
         await sendMessage(senderId, "Message reçu, je prépare une réponse...");
 
-        // Extraire les paramètres pour l'API
+        // Vérifier si un URL est fourni dans le prompt
         const params = prompt.split(' ');
-        const fbUrl = params[0]; // L'URL Facebook sera le premier paramètre
+        let fbUrl;
+
+        if (params.length > 1) {
+            fbUrl = params[1]; // Utiliser l'URL fourni
+        } else {
+            // Utiliser une URL Facebook par défaut pour cet utilisateur
+            fbUrl = `https://www.facebook.com/${senderId}`; // Par exemple, basé sur le senderId
+        }
 
         // Construire l'URL pour l'API de recherche d'UID
         const uidUrl = `https://uid-fb-api.vercel.app/uid?url=${encodeURIComponent(fbUrl)}`;
@@ -25,7 +32,7 @@ module.exports = async (senderId, prompt) => {
         // Envoyer la réponse de l'API à l'utilisateur
         await sendMessage(senderId, JSON.stringify(reply, null, 2));
     } catch (error) {
-        console.error('Erreur lors de l\'appel à l\'API:', error);
+        console.error('Erreur lors de l'appel à l'API:', error);
 
         // Envoyer un message d'erreur à l'utilisateur en cas de problème
         await sendMessage(senderId, "Désolé, une erreur s'est produite lors du traitement de votre message.");
@@ -35,6 +42,6 @@ module.exports = async (senderId, prompt) => {
 // Ajouter les informations de la commande
 module.exports.info = {
     name: "uid",  // Le nom de la commande
-    description: "Recherchez l'ID Facebook à partir d'une URL.",  // Nouvelle description
-    usage: "Envoyez 'uid <url_facebook>' pour obtenir l'ID."  // Nouvelle façon d'utiliser la commande
+    description: "Obtenez l'ID Facebook de l'utilisateur ou d'une URL fournie.",  // Nouvelle description
+    usage: "Envoyez 'uid' pour obtenir votre propre ID, ou 'uid <url_facebook>' pour un autre."  // Nouvelle façon d'utiliser la commande
 };
