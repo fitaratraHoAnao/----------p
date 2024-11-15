bd5d469bfbd43eec63a45e89f2c6b96bf2b5d5be
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2024-11-15 08:59:00 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 const sendMessage = require('../handles/sendMessage'); // Importer la fonction sendMessage
+const sendLongMessage = require('../handles/sendMessage').sendLongMessage;
 
 module.exports = async (senderId) => {
-    const commandsDir = path.join(__dirname);
-    const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
-    const commands = commandFiles.map(file => require(`./${file}`).info);
+    try {
+        const commandsDir = path.join(__dirname);
+        console.log('Chemin du répertoire des commandes :', commandsDir);
 
-    // Construire le message de réponse
-    let message = `🇲🇬 *Liste complète des commandes disponibles*:\n\n`;
-    commands.forEach((command, index) => {
-        message += `${index + 1}- ${command.name}\n`;
-        message += `  ✅ Description 👉: ${command.description}\n`;
-        message += `  ✅ Usage 👉: ${command.usage}\n\n`;
-    });
+        const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
+        console.log('Fichiers de commandes trouvés :', commandFiles);
 
-    // Envoyer le message
-    await sendMessage(senderId, message);
+        const commands = commandFiles.map(file => require(`./${file}`).info);
+        console.log('Commandes chargées dans help :', commands);
+
+        let message = `🇲🇬 *Liste complète des commandes disponibles*:\n\n`;
+        commands.forEach((command, index) => {
+            message += `${index + 1}- ${command.name}\n`;
+            message += `  ✅ Description 👉: ${command.description}\n`;
+            message += `  ✅ Usage 👉: ${command.usage}\n\n`;
+        });
+
+        await sendLongMessage(senderId, message);
+    } catch (error) {
+        console.error('Erreur dans la commande help :', error);
+        await sendMessage(senderId, 'Désolé, une erreur est survenue lors de l\'exécution de la commande help.');
+    }
 };
 
 // Ajouter les informations de la commande
